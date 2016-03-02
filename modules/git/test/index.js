bba0198ba481412aa384143bc4f1793f2b6cd21c
tkhank <tkssuetce@live.com>
2016-03-02 22:22:12 +0500

    git module files added

---
+++ modules/git/test/index.js
@@ -0,0 +1,17 @@
+
+var assert = require('assert');
+
+describe('Git', function() {
+  before(function(){
+    // The before() callback gets run before all tests in the suite. Do one-time setup here.
+  });
+  beforeEach(function(){
+    // The beforeEach() callback gets run before each test in the suite.
+  });
+  it('does x when y', function(){
+    // Now... Test!
+  });
+  after(function() {
+    // after() is run after all your tests have completed. Do teardown here.
+  });
+});
