229d2524c1286ff2ad1bec46978a6a11c9e872cd
tkhank <tkssuetce@live.com>
2016-03-07 01:25:26 +0500

    logs enabled boolean added and gittask test wip

--- test/gittask.js
+++ test/gittask.js
@@ -1,21 +1,38 @@
 'use strict';
 
 var assert = require('assert');
+var path = require('../helper/path');
+var Shell = require('../lib/Shell');
+var testground = `${path.stepBack(__dirname, 2)}/testground`;
 
-describe('Git Task', function() {
+describe('GitTask - clone', function(){
+  this.timeout(60000);
   before(function(){
-    // The before() callback gets run before all tests in the suite. Do one-time setup here.
+    Shell.cmd().exec(`rm -r ${testground}`);
   });
   beforeEach(function(){
-    // The beforeEach() callback gets run before each test in the suite.
-  });
-  it('does x when y', function(){
-    // Now... Test!
-		var shell = require('shelljs');
 
-		var c = shell.cd('/home/talha/Talha/Learning/mentos');
+  });
+  it('can clone public repo with http', function(done){
+    var GitTask = require('../lib/GitTask');
+    var creds = {
+      username: '',
+      password: ''
+    };
+    var opts = {
+      url: 'https://github.com/t-khan-k/angular-listview.git',
+      opType: 'clone',
+      folderPath: testground,
+      logsEnabled: false
+    };
+    var git = new GitTask(0, 'http clone', creds, opts);
+    git.start().then(()=>{
+      var result = Shell.cmd().exec(`cd ${testground} && ls | grep angular-listview`);
+      assert.equal(0, parseInt(result.code), 'result code should be 0');
+      done();
+    });
   });
   after(function() {
-    // after() is run after all your tests have completed. Do teardown here.
+
   });
 });
